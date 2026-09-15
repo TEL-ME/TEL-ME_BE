7fa1ad520749631d5b3b2827383b23dba8107543
seyeonham <seyeon021023@gmail.com>
2026-09-16 02:30:23 +0900

    chore: 기본 패키지 구조 생성

---
+++ src/main/java/com/telme/global/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 전역 공통 영역.
+ * <p>
+ * 공통 응답, 예외 처리, 보안 · Swagger · Resilience4j 등 애플리케이션 설정을 둔다.
+ * 특정 도메인 패키지를 참조하지 않는다.
+ */
+package com.telme.global;
---
+++ src/main/java/com/telme/member/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 회원 도메인.
+ * <p>
+ * 회원가입, 로그인, 소셜 로그인, 비회원(guestId) 식별을 담당한다.
+ */
+package com.telme.member;
