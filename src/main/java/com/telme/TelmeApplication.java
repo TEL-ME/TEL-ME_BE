7231bd2dce0768bec57569bb70949e13b692edf2
seyeonham <seyeon021023@gmail.com>
2026-09-16 01:55:14 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/telme/TelmeApplication.java
@@ -0,0 +1,13 @@
+package com.telme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelmeApplication.class, args);
+	}
+
+}
