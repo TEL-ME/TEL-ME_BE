0284d425a144d40d21838f3ae8e5cf657da4c8c1
shin838 <21stseptemberr@gmail.com>
2026-09-17 15:18:57 +0900

    TELME-19 fix: 재임베딩 더미 빈 충돌 방지

--- src/main/java/com/telme/faq/service/DummyFaqEmbeddingSyncService.java
+++ src/main/java/com/telme/faq/service/DummyFaqEmbeddingSyncService.java
@@ -1,14 +1,15 @@
 package com.telme.faq.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
-// TODO: 실제 구현(재임베딩 upsert/delete) 추가 시 이 클래스는 삭제할 것
 @Slf4j
 @Service
+@ConditionalOnMissingBean(FaqEmbeddingSyncService.class)
 public class DummyFaqEmbeddingSyncService implements FaqEmbeddingSyncService {
 
-	//호출되었다는 것만 로그로 확인
+    // 호출되었다는 것만 로그로 확인
     @Override
     public void upsert(Long faqId) {
         log.info("[Dummy] upsert faqId={}", faqId);
