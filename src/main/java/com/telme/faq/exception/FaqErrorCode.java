3d3c9d84f65bfbefb435ec52630e38960ed7fa35
shin838 <21stseptemberr@gmail.com>
2026-09-18 17:11:16 +0900

    TELME-21 refactor: FaqErrorCode 선언 순서를 상태코드 순으로 정렬

--- src/main/java/com/telme/faq/exception/FaqErrorCode.java
+++ src/main/java/com/telme/faq/exception/FaqErrorCode.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FaqErrorCode implements BaseErrorCode {
 
+    EMBEDDING_REQUEST_INVALID(HttpStatus.BAD_REQUEST, "FAQ400-0", "임베딩 요청이 올바르지 않습니다."),
     EMBEDDING_RESPONSE_INVALID(HttpStatus.INTERNAL_SERVER_ERROR, "FAQ500-0", "임베딩 응답이 올바르지 않습니다."),
     EMBEDDING_REQUEST_REJECTED(HttpStatus.INTERNAL_SERVER_ERROR, "FAQ500-1", "임베딩 요청이 Ollama에서 거부되었습니다."),
-    EMBEDDING_REQUEST_INVALID(HttpStatus.BAD_REQUEST, "FAQ400-0", "임베딩 요청이 올바르지 않습니다."),
     EMBEDDING_REQUEST_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "FAQ503-0", "임베딩 서버 호출에 실패했습니다.");
 
     private final HttpStatus status;
